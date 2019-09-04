95915c2f73df5e649690809b2ccc0b818543ecd1
Ewelina Gregoraszczuk <gregoraszczuk.e@gmail.com>
2019-09-04 18:58:51 +0200

    extracted method

--- src/main/java/com/example/podatek/service/UserInputService.java
+++ src/main/java/com/example/podatek/service/UserInputService.java
@@ -8,19 +8,26 @@ public class UserInputService {
     public static final int MAX_INCOME = 85528;
 
     public static String getIncomeFromUser() throws InputMismatchException {
-        TaxService taxService = new TaxService();
+
         Scanner input = new Scanner(System.in);
         double income = input.nextDouble();
         input.nextLine();
         input.close();
+
+        int tax = getTax(income);
+        return "Podatek do zapłaty (w zaokrągleniu do pełnych zł): " + tax;
+    }
+
+    private static int getTax(double income) {
+        TaxService taxService = new TaxService();
         int tax;
 
         if (income <= MAX_INCOME) {
             tax = taxService.countTaxForLessIncome((int) income);
-            return "Podatek do zapłaty (w zaokrągleniu do pełnych zł): " + tax;
+            return tax;
         } else {
             tax = taxService.countTaxForGreaterIncome((int) income);
-            return "Podatek do zapłaty (w zaokrągleniu do pełnych zł): " + tax;
+            return tax;
         }
     }
 }
